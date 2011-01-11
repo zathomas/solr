f507b6b3d3d296e59cc59a50333a64f8ae6ca690
Carl Hall <carl@hallwaytech.com>
2011-01-10 20:15:53 -0500

    Added lookup for existing logger configuration before creating a new one.

--- src/main/java/org/sakaiproject/nakamura/solr/EmbeddedSolrClient.java
+++ src/main/java/org/sakaiproject/nakamura/solr/EmbeddedSolrClient.java
@@ -14,6 +14,7 @@ import org.apache.solr.core.SolrConfig;
 import org.apache.solr.core.SolrCore;
 import org.apache.solr.schema.IndexSchema;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.ComponentContext;
@@ -37,6 +38,9 @@ import javax.xml.parsers.ParserConfigurationException;
 public class EmbeddedSolrClient implements SolrServerService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(EmbeddedSolrClient.class);
+
+  private static final String LOGGER_KEY = "org.sakaiproject.nakamura.logger";
+  private static final String LOGGER_VAL = "org.apache.solr";
   /**
    * According to the doc, this is thread safe and must be shared between all threads.
    */
@@ -54,17 +58,22 @@ public class EmbeddedSolrClient implements SolrServerService {
     BundleContext bundleContext = componentContext.getBundleContext();
     solrHome = Utils.getSolrHome(bundleContext);
     // Note that the following property could be set through JVM level arguments too
-    LOGGER
-        .info(" ================================== Creating new Logger for Embedded Solr");
-    Configuration logConfiguration = configurationAdmin.createFactoryConfiguration(
-        "org.apache.sling.commons.log.LogManager.factory.config", null);
+    LOGGER.info(" ============================== Creating Logger for Embedded Solr");
+    Configuration logConfiguration = getLogConfiguration();
 
-    Dictionary<String, Object> loggingProperties = new Hashtable<String, Object>();
-    loggingProperties.put("pid", this.getClass().getName());
-    loggingProperties.put("org.apache.sling.commons.log.level", "INFO");
-    loggingProperties.put("org.apache.sling.commons.log.file", "logs/solr.log");
-    loggingProperties.put("org.apache.sling.commons.log.names", "org.apache.solr");
-    logConfiguration.update(loggingProperties);
+    // create a log configuration if none was found. leave alone any found configurations
+    // so that modifications will persist between server restarts
+    if (logConfiguration == null) {
+      logConfiguration = configurationAdmin.createFactoryConfiguration(
+          "org.apache.sling.commons.log.LogManager.factory.config", null);
+      Dictionary<String, Object> loggingProperties = new Hashtable<String, Object>();
+      loggingProperties.put("org.apache.sling.commons.log.level", "INFO");
+      loggingProperties.put("org.apache.sling.commons.log.file", "logs/solr.log");
+      loggingProperties.put("org.apache.sling.commons.log.names", "org.apache.solr");
+      // add this property to give us something unique to re-find this configuration
+      loggingProperties.put(LOGGER_KEY, LOGGER_VAL);
+      logConfiguration.update(loggingProperties);
+    }
 
     System.setProperty("solr.solr.home", solrHome);
     File solrHomeFile = new File(solrHome);
@@ -92,6 +101,20 @@ public class EmbeddedSolrClient implements SolrServerService {
       Thread.currentThread().setContextClassLoader(contextClassloader);
     }
 
+  }
+
+  private Configuration getLogConfiguration() throws IOException {
+    Configuration logConfiguration = null;
+    try {
+      Configuration[] configs = configurationAdmin.listConfigurations("(" + LOGGER_KEY
+          + "=" + LOGGER_VAL + ")");
+      if (configs != null && configs.length > 0) {
+        logConfiguration = configs[0];
+      }
+    } catch (InvalidSyntaxException e) {
+      // ignore this as we'll create what we need
+    }
+    return logConfiguration;
   }
 
   private InputStream getStream(String name) {
