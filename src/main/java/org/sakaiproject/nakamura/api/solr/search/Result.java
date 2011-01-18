ce9881ac378136593d48ee9067ca77db4f513796
Ian Boston <ieb@tfd.co.uk>
2011-01-18 09:11:42 +0000

    Removed Solr Search Servlet to the Search bundle and eliminated dependency on the main Nakamura code base,

--- src/main/java/org/sakaiproject/nakamura/api/solr/search/Result.java
+++
@@ -1,7 +0,0 @@
-package org.sakaiproject.nakamura.api.solr.search;
-
-public interface Result {
-
-  String getPath();
-
-}
--- src/main/java/org/sakaiproject/nakamura/api/solr/search/SolrSearchServiceFactory.java
+++
@@ -1,9 +0,0 @@
-package org.sakaiproject.nakamura.api.solr.search;
-
-import org.apache.sling.api.SlingHttpServletRequest;
-
-public interface SolrSearchServiceFactory {
-
-  SolrSearchResultSet getSearchResultSet(SlingHttpServletRequest request, String query) throws SolrSearchException;
-
-}
